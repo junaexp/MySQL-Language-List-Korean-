a47e22ece834ba0e9c108ddb07f5c972b6913b71
Juna Panchev <parkbeomjun@naver.com>
2018-01-19 22:55:51 +0900

    Update lang.sql

--- lang.sql
+++ lang.sql
@@ -1,8 +1,9 @@
 /*
 SQLyog Ultimate v12.09 (64 bit)
-MySQL - 10.1.30-MariaDB-1~xenial : Database - uptempo-com
+MySQL - 10.1.30-MariaDB-1~xenial : Database - [YOUR DBNAME]
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,9 +13,9 @@ MySQL - 10.1.30-MariaDB-1~xenial : Database - uptempo-com
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`uptempo-com` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`[YOUR DBNAME]` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 
-USE `uptempo-com`;
+USE `[YOUR DBNAME]`;
 
 /*Table structure for table `lang` */
 
